3807e37be25bad7ed3997102de1ee7979837b45f
Imtious-Coder <iarafat386@gmail.com>
2025-07-01 02:51:15 +0600

    :art: Notification UI completed.

--- app/components/Admin/DashboardHeader.tsx
+++ app/components/Admin/DashboardHeader.tsx
@@ -21,7 +21,35 @@ const DashboardHeader: FC<Props> = () => {
         </span>
       </div>
       {open && (
-        <div className="w-[350px] h-[50vh] bg-[#111C43] shadow-xl absolute top-16 z-10 rounded "></div>
+        <div className="w-[350px] h-[50vh] bg-[#111C43] shadow-xl absolute top-16 z-10 rounded ">
+          <h5 className="text-center text-[20px] fornt-Poppins text-white p-3 ">
+            Notifications
+          </h5>
+          <div className="bg-[#2d3a4ea1] font-Poppins border-b border-b-[#ffffff47] ">
+            <div className="w-full flex items-center justify-between p-2">
+              <p className="text-white">New Question Received</p>
+              <p className="text-white  cursor-pointer">Mark as read</p>
+            </div>
+            <p className="px-2 text-white">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam,
+              eos ex doloremque quis quisquam nobis necessitatibus qui numquam
+              pariatur autem.
+            </p>
+            <p className="text-white p-2 text-[14px]">5 days ago</p>
+          </div>
+          <div className="bg-[#2d3a4ea1] font-Poppins border-b border-b-[#ffffff47] ">
+            <div className="w-full flex items-center justify-between p-2">
+              <p className="text-white">New Question Received</p>
+              <p className="text-white  cursor-pointer">Mark as read</p>
+            </div>
+            <p className="px-2 text-white">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam,
+              eos ex doloremque quis quisquam nobis necessitatibus qui numquam
+              pariatur autem.
+            </p>
+            <p className="text-white p-2 text-[14px]">5 days ago</p>
+          </div>
+        </div>
       )}
     </div>
   );
